015748701c33dda5707494164e8764d5608301f7
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-30 20:49:27 +0000

    nothign work

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once "baseConstants.php";
+
+const CONTROLLERS = BASE_PATH . "/controllers/";
+const MODELS = BASE_PATH . "/models/";
+const VIEWS = BASE_PATH . "/views/";
+const RESOURCES = BASE_PATH . "/resources/";
---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+
+const HOST = 'localhost';
+const DB = 'employees_v2';
+const USER = 'root';
+const PASSWORD = '';
+const CHARSET = 'utf8mb4';
